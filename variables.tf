ceeddd5bc90ad2766dd7efe7756922f1151b74d8
Manoj Chaudhary <your_github_email@example.com>
2026-06-27 03:16:39 +0000

    modified:   variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,33 @@
-variable "region" {
-  default = "ap-south-1"
+variable "aws_region" {
+  description = "AWS region where resources will be created"
+  type        = string
+  default     = "ap-south-1"
 }
 
-variable "ami" {
-  description = "Amazon Linux 2023 AMI"
+variable "ami_id" {
+  description = "Ubuntu EC2 AMI ID"
+  type        = string
 }
 
 variable "instance_type" {
-  default = "t3.micro"
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
 }
 
-variable "key_name" {}
+variable "key_name" {
+  description = "Existing AWS EC2 Key Pair name"
+  type        = string
+}
 
-variable "public_key_path" {}
+variable "instance_name" {
+  description = "Name tag for the EC2 instance"
+  type        = string
+  default     = "tf-single-ec2"
+}
+
+variable "security_group_name" {
+  description = "Security Group name"
+  type        = string
+  default     = "tf-single-ec2-sg"
+}
